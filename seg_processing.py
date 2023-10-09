845c0fc40ad1f421cebc6b8fd80b7a902bb53adb
SLE_formation <st_lerendu@yahoo.fr>
2023-10-09 12:10:59 +0200

    refacto size

--- seg_processing.py
+++ seg_processing.py
@@ -1,117 +1,10 @@
-import cv2
-import matplotlib.pyplot as plt
-import numpy as np
-from matplotlib import colors
-from collections import namedtuple
-from PIL import Image
 import keras
 from segmentation_models.metrics import iou_score
 from segmentation_models.losses import dice_loss
 
 model_file_name = "models/07_last_optim.h5"
 
-#
-Label = namedtuple( 'Label' , [
-
-    'name'        , # The identifier of this label, e.g. 'car', 'person', ... .
-                    # We use them to uniquely name a class
-
-    'id'          , # An integer ID that is associated with this label.
-                    # The IDs are used to represent the label in ground truth images
-                    # An ID of -1 means that this label does not have an ID and thus
-                    # is ignored when creating ground truth images (e.g. license plate).
-                    # Do not modify these IDs, since exactly these IDs are expected by the
-                    # evaluation server.
-
-    'trainId'     , # Feel free to modify these IDs as suitable for your method. Then create
-                    # ground truth images with train IDs, using the tools provided in the
-                    # 'preparation' folder. However, make sure to validate or submit results
-                    # to our evaluation server using the regular IDs above!
-                    # For trainIds, multiple labels might have the same ID. Then, these labels
-                    # are mapped to the same class in the ground truth images. For the inverse
-                    # mapping, we use the label that is defined first in the list below.
-                    # For example, mapping all void-type classes to the same ID in training,
-                    # might make sense for some approaches.
-                    # Max value is 255!
-
-    'category'    , # The name of the category that this label belongs to
-
-    'categoryId'  , # The ID of this category. Used to create ground truth images
-                    # on category level.
-
-    'hasInstances', # Whether this label distinguishes between single instances or not
-
-    'ignoreInEval', # Whether pixels having this class as ground truth label are ignored
-                    # during evaluations or not
-
-    'color'       , # The color of this label
-    ] )
-
-labels = [
-    #       name                     id    trainId   category            catId     hasInstances   ignoreInEval   color
-    Label(  'unlabeled'            ,  0 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'ego vehicle'          ,  1 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'rectification border' ,  2 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'out of roi'           ,  3 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'static'               ,  4 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
-    Label(  'dynamic'              ,  5 ,      255 , 'void'            , 0       , False        , True         , (111, 74,  0) ),
-    Label(  'ground'               ,  6 ,      255 , 'void'            , 0       , False        , True         , ( 81,  0, 81) ),
-    Label(  'road'                 ,  7 ,        0 , 'flat'            , 1       , False        , False        , (128, 64,128) ),
-    Label(  'sidewalk'             ,  8 ,        1 , 'flat'            , 1       , False        , False        , (244, 35,232) ),
-    Label(  'parking'              ,  9 ,      255 , 'flat'            , 1       , False        , True         , (250,170,160) ),
-    Label(  'rail track'           , 10 ,      255 , 'flat'            , 1       , False        , True         , (230,150,140) ),
-    Label(  'building'             , 11 ,        2 , 'construction'    , 2       , False        , False        , ( 70, 70, 70) ),
-    Label(  'wall'                 , 12 ,        3 , 'construction'    , 2       , False        , False        , (102,102,156) ),
-    Label(  'fence'                , 13 ,        4 , 'construction'    , 2       , False        , False        , (190,153,153) ),
-    Label(  'guard rail'           , 14 ,      255 , 'construction'    , 2       , False        , True         , (180,165,180) ),
-    Label(  'bridge'               , 15 ,      255 , 'construction'    , 2       , False        , True         , (150,100,100) ),
-    Label(  'tunnel'               , 16 ,      255 , 'construction'    , 2       , False        , True         , (150,120, 90) ),
-    Label(  'pole'                 , 17 ,        5 , 'object'          , 3       , False        , False        , (153,153,153) ),
-    Label(  'polegroup'            , 18 ,      255 , 'object'          , 3       , False        , True         , (153,153,153) ),
-    Label(  'traffic light'        , 19 ,        6 , 'object'          , 3       , False        , False        , (250,170, 30) ),
-    Label(  'traffic sign'         , 20 ,        7 , 'object'          , 3       , False        , False        , (220,220,  0) ),
-    Label(  'vegetation'           , 21 ,        8 , 'nature'          , 4       , False        , False        , (107,142, 35) ),
-    Label(  'terrain'              , 22 ,        9 , 'nature'          , 4       , False        , False        , (152,251,152) ),
-    Label(  'sky'                  , 23 ,       10 , 'sky'             , 5       , False        , False        , ( 70,130,180) ),
-    Label(  'person'               , 24 ,       11 , 'human'           , 6       , True         , False        , (220, 20, 60) ),
-    Label(  'rider'                , 25 ,       12 , 'human'           , 6       , True         , False        , (255,  0,  0) ),
-    Label(  'car'                  , 26 ,       13 , 'vehicle'         , 7       , True         , False        , (  0,  0,142) ),
-    Label(  'truck'                , 27 ,       14 , 'vehicle'         , 7       , True         , False        , (  0,  0, 70) ),
-    Label(  'bus'                  , 28 ,       15 , 'vehicle'         , 7       , True         , False        , (  0, 60,100) ),
-    Label(  'caravan'              , 29 ,      255 , 'vehicle'         , 7       , True         , True         , (  0,  0, 90) ),
-    Label(  'trailer'              , 30 ,      255 , 'vehicle'         , 7       , True         , True         , (  0,  0,110) ),
-    Label(  'train'                , 31 ,       16 , 'vehicle'         , 7       , True         , False        , (  0, 80,100) ),
-    Label(  'motorcycle'           , 32 ,       17 , 'vehicle'         , 7       , True         , False        , (  0,  0,230) ),
-    Label(  'bicycle'              , 33 ,       18 , 'vehicle'         , 7       , True         , False        , (119, 11, 32) ),
-    Label(  'license plate'        , -1 ,       -1 , 'vehicle'         , 7       , False        , True         , (  0,  0,142) ),
-]
-
-#Affichage des catégories et les labels associés
-id_category     = { label[4]   : label.category for label in labels  }
-
-def generate_img_from_mask(mask,colors_palette=['b','g','r','c','m','y','k','w']):
-
-    '''Génréation d'une image PIL depuis un masque segmenté et catégorie avec une couleur associée à chacune des catégories
-
-    Args:
-      mask - numpy array of dimension (shape(img),len(cats))
-      cats - dict {'cat1':[value1,value2,value3],'cat2':[value1,value2,value3]}
-
-    Returns
-      PIL image'''
-    img_seg = np.zeros((mask.shape[0],mask.shape[1],3),dtype='float')
-
-    print("mask size :",mask.shape)
-    print("img_seg size :",img_seg.shape)
-
-    for cat in id_category.keys():
-        img_seg[:,:,0] += mask[:,:,cat]*colors.to_rgb(colors_palette[cat])[0]
-        img_seg[:,:,1] += mask[:,:,cat]*colors.to_rgb(colors_palette[cat])[1]
-        img_seg[:,:,2] += mask[:,:,cat]*colors.to_rgb(colors_palette[cat])[2]
-
-    return img_seg
-
-def affichage_result_mask(_img, upload_dir):
+def affichage_result_mask(_img):
 
     # load the model and detect
     loaded_model = keras.models.load_model(model_file_name ,
@@ -120,18 +13,6 @@ def affichage_result_mask(_img, upload_dir):
                                         "iou_score": iou_score
                                         })
 
-    img_test_array  = cv2.imread(_img)
+    mask_u = loaded_model.predict(_img)
 
-    resized = cv2.resize(img_test_array,(6*48,3*48))
-    resized = np.expand_dims(resized, axis = 0)
-
-    mask_u = loaded_model.predict(resized)
-
-    print("mask_u.shape before squeeze: ",mask_u.shape)
-    a_s = np.squeeze(mask_u)
-    mask_t = generate_img_from_mask(a_s)
-
-    img = Image.fromarray(np.uint8(mask_t * 255))
-    img.save(upload_dir + "/" + 'masked_pic.png')
-
-    return mask_t
+    return mask_u
